f415c9e5d509dbce8217176af391a9cf0401b04b
dhyun <ehdghho@kakao.com>
2023-03-07 13:43:52 +0900

    Add require pkg install command in Dockerfile

--- srcs/requirements/tools/clean.sh
+++ srcs/requirements/tools/clean.sh
@@ -4,4 +4,4 @@ docker stop mariadb wordpress nginx;
 docker rm mariadb wordpress nginx;
 docker rmi $(docker images -q);
 docker volume rm $(docker volume ls -q);
-docker network rm $(docker network ls -q) 2 > /dev/null;
+docker network rm $(docker network ls -q);
