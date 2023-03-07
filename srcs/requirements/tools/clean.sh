ad7e9bb36967f05be540657c0d0c3b849c6f4d62
dhyun <ehdghho@kakao.com>
2023-03-07 14:18:48 +0900

    fix clean.sh

--- srcs/requirements/tools/clean.sh
+++ srcs/requirements/tools/clean.sh
@@ -4,4 +4,4 @@ docker stop mariadb wordpress nginx;
 docker rm mariadb wordpress nginx;
 docker rmi $(docker images -q);
 docker volume rm $(docker volume ls -q);
-docker network rm mynet;
+docker network rm srcs_mynet;
