d419e7b1c64bd1152129415f57b86e19ae127b59
dhyun <ehdghho@kakao.com>
2023-03-07 12:46:21 +0900

    mk clean.sh fix mariaDB Dockerfile

---
+++ srcs/requirements/tools/clean.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker stop mariadb wordpress nginx
+docker rm mariadb wordpress nginx
+docker rmi $(docker images -q)
+docker volume rm $(docker volume ls -q)
+docker network rm $(docker network ls -q)
